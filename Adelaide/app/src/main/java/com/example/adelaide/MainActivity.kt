71ee3a5e94941ccb5c1e41b2af5fd24d4083caa9
ctellez28 <65038341+ctellez28@users.noreply.github.com>
2020-05-08 11:12:54 -0700

    Add files via upload

---
+++ Adelaide/app/src/main/java/com/example/adelaide/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.adelaide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
